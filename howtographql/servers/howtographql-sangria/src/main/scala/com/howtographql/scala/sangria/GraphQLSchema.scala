c6943ee6e035ea712af764fc4636f10511ce6a1a
emag <lanabe.lanabe@gmail.com>
2018-08-16 00:23:47 +0900

    Done 3-handling_arguments

--- howtographql/servers/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ howtographql/servers/howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -18,10 +18,23 @@ object GraphQLSchema {
 
   implicit val LinkType = deriveObjectType[Unit, Link]()
 
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
+
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.dao.getLink(c.arg[Int]("id"))
+      ),
+      Field("links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg[Seq[Int]]("ids"))
+      )
     )
   )
 
